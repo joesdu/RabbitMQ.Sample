c8bcffe539ea8c21f263e16ce02334e867682e66
Joes <dygood@outlook.com>
2022-04-13 15:48:45 +0800

    添加WorkQueues模式的测试代码

---
+++ 02-WorkQueues/WorkQueues.Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Sample.Common;
+using System.Text;
+
+// 该启动模式请从编译后的exe文件中启动,可启动多个实例来体现多消费者的情况.
+
+Console.WriteLine("RabbitMQ WorkQueues Model,Consumer!");
+
+using var connection = RabbitHelper.GetFactory().CreateConnection();
+using var channel = connection.CreateModel();
+
+_ = channel.QueueDeclare(queue: "work_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var consumer = new EventingBasicConsumer(channel);
+var rand = new Random();
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Consumer Received {message}");
+    var second = rand.Next(1, 3);
+    Thread.Sleep(second * 1000);
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+_ = channel.BasicConsume(queue: "work_queue", autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ 02-WorkQueues/WorkQueues.Provider/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using Sample.Common;
+using System.Text;
+
+Console.WriteLine("RabbitMQ WorkQueues Model,Provider!");
+
+using var connection = RabbitHelper.GetFactory().CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "work_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+const int max = 30;
+int i = 0;
+while (true && i < max)
+{
+    var message = $"Hello WorkQueues-{i}";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "", routingKey: "work_queue", basicProperties: properties, body: body);
+    Console.WriteLine($" [x] Sent {message}");
+    i++;
+    Thread.Sleep(1000);
+}
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
